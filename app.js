43f009f24202744cbe80992a736aeaaea4a1a2f4
xiao7728050 <xiao7728050@users.noreply.github.com>
2026-07-14 17:35:02 +0800

    feat: add Sanofi customer account

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ const PARTNER_HASHES = {
   "e1b987bd53562c4a8ce38c4853de12b672be1dd7419b82e524524fac9a078fdf": { name: "Atos", type: "partner" },
   "bb98a8eebf3192ad54df4b02c1608d69fed6b1602db8522a8f5d6d6301fbb900": { name: "TCS", type: "partner" },
   "387536999142a55f4d66bbe633d6688840aa7c08bbcb3d4d5423bc4729151835": { name: "AWS", type: "partner" },
-  "dcd7bd716bc4812045196aec8cff0f94c159b56784a08f4c0cbeba0a43df7710": { name: "信泰宜合", type: "partner" }
+  "dcd7bd716bc4812045196aec8cff0f94c159b56784a08f4c0cbeba0a43df7710": { name: "信泰宜合", type: "partner" },
+  "dd8f4e7aae4b87e0ef31198cd9fd406208e53e94afe9487131ed0f99ce000964": { name: "Sanofi", type: "customer" }
 };
 
 let trackCount = 0;
